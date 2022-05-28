0876966e31e36038b719d6651f36cb9bcc4f66e6
Enrique Teruel <enriqueteruel@MacBook-Air-de-Enrique.local>
2022-05-29 01:49:59 +0200

    feat(init): structure and routing

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppRouter from './components/router/AppRouter';
+
+function App() {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Login from '../auth/LoginScreen';
+import CalendarScreen from '../calendar/CalendarScreen';
+import NotFound from '../ui/notFound';
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<CalendarScreen />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/calendar" element={<CalendarScreen />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
